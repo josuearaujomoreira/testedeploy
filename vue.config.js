d1088a0bd1059e169c323103a7e23c46a7581c74
josuearaujomoreira <66573553+josuearaujomoreira@users.noreply.github.com>
2025-03-03 09:43:25 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/',
+  
+  // Adicione a configuração do devServer aqui
+  devServer: {
+    host: '0.0.0.0',  // Isso permite conexões de fora do container
+    port: 3000        // A porta que o Vue irá escutar dentro do container
+  },
 
   pluginOptions: {
     i18n: {
